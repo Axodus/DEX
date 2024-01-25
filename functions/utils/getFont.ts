d008b2c134343b4781125aafa6ba7ef0b8d63e08
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-01-25 18:39:18 -0300

    fonts update

--- functions/utils/getFont.ts
+++ functions/utils/getFont.ts
@@ -1,5 +1,5 @@
 export default async function getFont(origin: string) {
-  const url = origin + '/fonts/Inter-normal.var.ttf'
+  const url = origin + '/fonts/Ubuntu-Regular.woff'
   const font = await fetch(url)
   return font.arrayBuffer()
 }
